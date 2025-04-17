64a9de92ca6366af3f3289f265630311539fcc60
JoseLuisArce <arcade87@hotmail.com>
2025-04-17 00:41:23 -0500

    feat: aplicacion completa conectada a GSheets

--- lista.py
+++ lista.py
@@ -26,27 +26,41 @@ def update_gift_status(worksheet, index, new_status):
 st.title("Lista de Regalos para AXEL & MAXI")
 st.markdown("¡Prepárense para una doble dosis de amor! Los gemelos ya casi están aquí, ¡y nuestra emoción no cabe en el pecho!")
 st.markdown("Su cariño es el regalo más grande, y para ayudarnos a darles la bienvenida a esta familia, hemos preparado una lista de cositas especiales para estos dos pequeños aventureros.")
-st.markdown("Si gustan participar en esta emocionante etapa, los invitamos con mucha alegría a explorar nuestra lista, elegir un regalo, ir al enlace de compra y confirmanos su compra.")
+st.markdown("Si gustan participar en esta emocionante etapa, los invitamos con mucha alegría a explorar nuestra lista, elegir un regalo, ir al enlace de compra y confirmanos con un Regalado! =)")
 st.markdown("Estaremos felices de recibir sus obsequios el 02 de mayo en esta dirección: Jr. Paucartambo 285 Independencia, Referencia a la Altura de la Comisaria de Tahuantinsuyo.")
 st.markdown("¡Si no pueden ese día, no se preocupen! Cualquier otro día será genial. Solo recuerden que el 03 de mayo estaremos celebrando el Baby Shower fuera de casa.")
-st.markdown("Ojo los enlaces de compra de Falabella son de referencia, si ves el mismo producto en otra tienda también nos lo puedes enviar y nos olvides marcar tu regalo como Comprado Felicidades!")
+st.markdown("Ojo los enlaces de compra de Falabella son referenciales, si ves el mismo producto en otra tienda no dudes en comprarlo, avísanos si debemos ir por ellos.")
 st.markdown("Cualquier duda o mensaje no dudes en escribirnos al Whatsapp de Claudine 940959197 o Jose Luis 986109315.")
 st.markdown("¡Gracias por ser parte de esta maravillosa aventura gemelar!")
 st.write("---")
 
 st.markdown(
-    """
+    f"""
     <style>
-    div[data-baseweb="select"] > div {
-        margin-bottom: 0px !important; /* Reducir el margen inferior del selectbox */
-    }
-    div.stButton > button {
-        margin-top: 1.7em !important; /* Añadir un margen superior al botón */
+    .stApp {{
+        background-image: url("https://i.postimg.cc/7Ys2s4P2/Fondo-Pantalla-BShower.png") !important;
+        background-size: cover !important;
+        background-repeat: no-repeat !important;
+        background-position: center !important;        
+    }}   
+    h1 {{
+        color: white !important;
+    }}
+    .stMarkdown, .stText {{
+        color: white !important;
+    }}  
+    div.stButton button{{
+        margin-top: 0em !important;
         padding-top: 0px !important;
         padding-bottom: 0px !important;
-        height: auto !important; /* Ajustar la altura automáticamente */
-        line-height: 1 !important; /* Reducir el espaciado de la línea */
-    }
+        height: auto !important;
+        line-height: 1 !important;
+        background-color: green;
+        color: white;
+        border-radius: 5px;
+        border: none;
+        padding: 10px 20px;
+    }}
     </style>
     """,
     unsafe_allow_html=True,
@@ -55,7 +69,7 @@ st.markdown(
 df, worksheet = get_gsheet_data()
 
 for index, row in df.iterrows():
-    col1, col2, col3, col4, col5, col6 = st.columns([0.4,2.2,1.4,1,1.8,1.1])
+    col1, col2, col3, col4, col5, col6 = st.columns([0.4,2,1.4,1,1.8,1.2])
 
     with col1:
         st.write(f"**{row['Orden']}**")
@@ -68,15 +82,16 @@ for index, row in df.iterrows():
     with col5:
         status = row['Nos_confirmas_tu_regalo?']
         options = ["no", "Regalado! =)"]
-        default_index = 0 if "-" in status else 1
+        default_index = 0 if "no" in status else 1
         new_status = st.selectbox(
-            "Estado Guardado:",
+            "Estado:",
             options,
             index=default_index,
-            key=f"status_selectbox_{index}"
+            key=f"status_selectbox_{index}",
+            label_visibility="collapsed"
         )
-        # Almacenar el nuevo estado en la sesión para que persista al hacer clic en el botón
         st.session_state[f"new_status_{index}"] = new_status
+        # Almacenar el nuevo estado en la sesión para que persista al hacer clic en el botón
     with col6:
         if st.button("Guardar", key=f"save_button_{index}"):
             old_status = row['Nos_confirmas_tu_regalo?']
@@ -87,4 +102,4 @@ for index, row in df.iterrows():
                 st.rerun() # Volver a ejecutar para actualizar la vista
 
 st.write("---")
-st.markdown("¡Gracias por tu generosidad!")
+st.markdown("¡Gracias por tu gemelosidad!")
